a2562e9324bd4109fcf7141dedcae56d5a650906
RYAN\Ryan <rjtwoh01@louisville.edu>
2017-11-10 00:59:28 -0500

    Initial Commit
    A couple of lectures into tutorial, basic sandbox has been created.
    Character can run around

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    //We do this in late update to make sure that the camera position is calculated after all other updates are done
+    //That way, the camera position is calculated after the the player position is finished being calculated in update()
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position; //camera's position is set to the player's position
+    }
+}
---
+++ Assets/CameraRaycaster.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class CameraRaycaster : MonoBehaviour
+{
+    public Layer[] layerPriorities = {
+        Layer.Enemy,
+        Layer.Walkable
+    };
+
+    //SerializeField allows the Unity inspector to set this value
+    //But it keeps the variable private to prevent other classes from altering it
+    [SerializeField]float distanceToBackground = 100f;
+    Camera viewCamera;
+
+    RaycastHit m_hit;
+    public RaycastHit hit
+    {
+        get { return m_hit; }
+    }
+
+    Layer m_layerHit;
+    public Layer layerHit
+    {
+        get { return m_layerHit; }
+    }
+
+    void Start() // TODO Awake?
+    {
+        viewCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        // Look for and return priority layer hit
+        foreach (Layer layer in layerPriorities)
+        {
+            var hit = RaycastForLayer(layer);
+            if (hit.HasValue)
+            {
+                m_hit = hit.Value;
+                m_layerHit = layer;
+                return;
+            }
+        }
+
+        // Otherwise return background hit
+        m_hit.distance = distanceToBackground;
+        m_layerHit = Layer.RaycastEndStop;
+    }
+
+    RaycastHit? RaycastForLayer(Layer layer)
+    {
+        // 1 << variable means bit shift by 1
+        //so we're bit shifting the layer
+        //from 00001 to 00010 for example where 00001 is layer 1 and 00010 is layer 2
+        int layerMask = 1 << (int)layer; // See Unity docs for mask formation
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit; // used as an out parameter
+        bool hasHit = Physics.Raycast(ray, out hit, distanceToBackground, layerMask);
+        if (hasHit)
+        {
+            return hit;
+        }
+        return null;
+    }
+}
