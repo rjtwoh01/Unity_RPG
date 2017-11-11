0a6043e3a37494443b054c2cabe1693682e1de8e
RYAN\Ryan <rjtwoh01@louisville.edu>
2017-11-10 22:11:53 -0500

    Reorganize and cursor changes

---
+++ Assets/Camera & UI/CursorAffordance.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour {
+
+    [SerializeField] Texture2D walkCursor = null;
+    [SerializeField] Texture2D attackCursor = null;
+    [SerializeField] Texture2D questionCursor = null;
+    [SerializeField] Vector2 cursorHotspot = new Vector2(96, 96);//deminsion of texture
+    CameraRaycaster cameraRayCaster;
+
+	// Use this for initialization
+	void Start () {
+        cameraRayCaster = GetComponent<CameraRaycaster>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (cameraRayCaster.layerHit == Layer.Walkable)
+            Cursor.SetCursor(walkCursor, cursorHotspot, CursorMode.Auto);
+        else if (cameraRayCaster.layerHit == Layer.Enemy)
+            Cursor.SetCursor(attackCursor, cursorHotspot, CursorMode.Auto);
+        else //the "unknown" out of map
+            Cursor.SetCursor(questionCursor, cursorHotspot, CursorMode.Auto);
+    }
+}
