6880b958dcee2ed72da3663b9c612d1fcfd3e219
RYAN\Ryan <rjtwoh01@louisville.edu>
2017-11-12 19:33:59 -0500

    Fixed walking bug

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -62,7 +62,8 @@ public class PlayerMovement : MonoBehaviour
                     print("Unexpected layer found");
                     return;
             }
-        }        
+        }
+        WalkToDestination();
     }
 
     private void WalkToDestination()
