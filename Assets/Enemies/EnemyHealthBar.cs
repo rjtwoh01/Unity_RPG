79178d89753a7cd29ae5ebd884fce77e1cc62715
RYAN\Ryan <rjtwoh01@louisville.edu>
2018-02-15 09:08:37 -0500

    Player and enemy health bars - removed large enemy

--- Assets/Enemies/EnemyHealthBar.cs
+++ Assets/Enemies/EnemyHealthBar.cs
@@ -3,24 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(RawImage))]
-public class PlayerHealthBar : MonoBehaviour
+public class EnemyHealthBar : MonoBehaviour
 {
-
-    RawImage healthBarRawImage;
-    Player player;
+    RawImage healthBarRawImage = null;
+    Enemy enemy = null;
 
     // Use this for initialization
     void Start()
     {
-        player = FindObjectOfType<Player>();
+        enemy = GetComponentInParent<Enemy>(); // Different to way player's health bar finds player
         healthBarRawImage = GetComponent<RawImage>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xValue = -(player.healthAsPercentage / 2f) - 0.5f;
+        float xValue = -(enemy.healthAsPercentage / 2f) - 0.5f;
         healthBarRawImage.uvRect = new Rect(xValue, 0f, 0.5f, 1f);
     }
 }
